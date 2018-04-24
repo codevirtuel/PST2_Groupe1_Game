6d224f3b24fcab48ac0a0e1d60b61b2a69f81af3
DAUDIN <DAUDIN@DESKTOP-CB6PHIH>
2018-04-24 14:58:26 +0200

    Inutile Juste ligne css enlever

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,18 +1,21 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			VBox root;
+			FXMLLoader rootLoader = new FXMLLoader(getClass().getResource("view/Jeu - Accueil.fxml"));
+			root=rootLoader.load();
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
