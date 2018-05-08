e393da66f3ce624ee4687762fa144751b691c398
Baptiste <Baptiste@DESKTOP-2H3TG4E.home>
2018-05-08 17:20:42 +0200

    import application.database

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
-	
+
+import application.database.Connect;
 import application.view.accueilController;
 import application.view.finPartieController;
 import application.view.gameController;
@@ -11,34 +12,36 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
-
 public class Main extends Application {
-	
+
 	public static int width = 1280;
 	public static int height = 720;
-	
+
 	@Override
 	public void start(Stage primaryStage) {
+
 		try {
 			VBox root = new VBox();
-			
-			//gameController.primaryStage = primaryStage;
-			//finPartieController.primaryStage = primaryStage;
+
+			// gameController.primaryStage = primaryStage;
+			// finPartieController.primaryStage = primaryStage;
 			accueilController.primaryStage = primaryStage;
 			root = FXMLLoader.load(getClass().getResource("view/Jeu - Accueil.fxml"));
-			Scene scene = new Scene(root,width,height);
+			Scene scene = new Scene(root, width, height);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
+
 			primaryStage.setResizable(false);
 
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+
 	}
+
 }
