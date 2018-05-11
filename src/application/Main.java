bec164f91adf1269238645a474cef2a9b02600c3
DAUDIN <DAUDIN@DESKTOP-CB6PHIH>
2018-05-11 21:40:09 +0200

    test

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,14 +5,9 @@ import application.view.finPartieController;
 import application.view.gameController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-<<<<<<< HEAD
-=======
-import javafx.scene.text.Font;
->>>>>>> branch 'master' of https://github.com/codevirtuel/PST2_Groupe1_Game
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -23,12 +18,6 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-<<<<<<< HEAD
-			VBox root;
-			FXMLLoader rootLoader = new FXMLLoader(getClass().getResource("view/Jeu - Accueil.fxml"));
-			root=rootLoader.load();
-			Scene scene = new Scene(root,400,400);
-=======
 			VBox root = new VBox();
 			
 			//gameController.primaryStage = primaryStage;
@@ -39,8 +28,6 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			
 			primaryStage.setResizable(false);
-
->>>>>>> branch 'master' of https://github.com/codevirtuel/PST2_Groupe1_Game
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
