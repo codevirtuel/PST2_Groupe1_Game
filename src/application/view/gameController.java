e7d8eeb7dd71ce2d43f603dfbb2cd53511a09705
Alexis Poupelin <codevirtuel@gmail.com>
2018-06-07 18:09:07 +0200

    Added polygon detection and image rescaling

--- src/application/view/gameController.java
+++ src/application/view/gameController.java
@@ -14,6 +14,7 @@ import application.gestionThemes.Zone;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -31,6 +32,8 @@ public class gameController {
 	public static String nomTheme;
 	
 	private Theme theme = new Theme(nomTheme);
+	
+	private List<Zone> selectedZone = new ArrayList<Zone>();
 	
 	@FXML VBox vbox;
 	
@@ -95,9 +98,23 @@ public class gameController {
 
 			theme.setImageFond(new Image(URL));
 		}
-		BackgroundImage bgImage = new BackgroundImage(theme.getImageFond(),
+		double factor = Scaler.getFactor();
+		int imageWidth = (int) (480*factor);
+		int imageHeight = (int) (270*factor);
+		
+		Image newImage = scale(theme.getImageFond(),imageWidth,imageHeight,false);
+		
+		BackgroundImage bgImage = new BackgroundImage(newImage,
 				BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, new BackgroundSize(480, 270, false, false, false, true));
 		image.setBackground(new Background(bgImage));
+	}
+	
+	public Image scale(Image source, int targetWidth, int targetHeight, boolean preserveRatio) {
+	    ImageView imageView = new ImageView(source);
+	    imageView.setPreserveRatio(preserveRatio);
+	    imageView.setFitWidth(targetWidth);
+	    imageView.setFitHeight(targetHeight);
+	    return imageView.snapshot(null, null);
 	}
 	
 	public void showZones() {
@@ -106,7 +123,7 @@ public class gameController {
 			for(int i=0;i<z.getPoints().size();i++) {
 				z.getPoints().set(i, z.getPoints().get(i));
 			}
-			
+			z.setId(""+z.getIndex());
 			z.setOpacity(2.0);
 			z.setFill(Color.RED);
 			z.setStroke(Color.BLACK);
@@ -114,6 +131,14 @@ public class gameController {
 			image.getChildren().add(z);
 		}
 		System.out.println(image.getChildren());
+	}
+	
+	@FXML
+	public void clickOnZone(MouseEvent e) {
+		String index = e.getPickResult().getIntersectedNode().getId();
+		if(!index.equals("image")) {
+			System.out.println("Polygon detected!");
+		}
 	}
 	
 }
